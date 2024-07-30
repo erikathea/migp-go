084994bc80dd22fb86c8d86100243b8ae3c8c08e
erikathea <ethajes@gmail.com>
2024-07-30 16:42:29 -0300

    added compute_loglikelihood flag

--- run_pagpassgpt.sh
+++ run_pagpassgpt.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source ~/myvenv/bin/activate
 cd ~/src/PagPassGPT/
-python generate_pw_variant.py --input_password="$1" --generate_num="$2"
+python generate_pw_variant.py --input_password="$1" --generate_num="$2" --compute_loglikelihood
 deactivate
